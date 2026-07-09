1f11e8e4300444a5b0abff1271b5e83c6cf81957
lhetl <hyun91000@gmail.com>
2026-07-09 21:59:42 +0900

    :recycle: Exception 코드 수정

--- src/crawler_api/exception/FetchValueException.py
+++ src/crawler_api/exception/FetchValueException.py
@@ -2,6 +2,10 @@ from http.client import HTTPException
 
 
 class FetchValueException(HTTPException):
-    def __init__(self):
-        super().__init__("Fetch 과정에서 문제가 발생했습니다")
+    def __init__(self, message : str | None = None):
         self.status_code = 404
+        if message:
+            super().__init__(message)
+        else:
+            super().__init__("Fetch 과정에서 문제가 발생했습니다")
+
--- src/crawler_api/exception/SelectorValueException.py
+++ src/crawler_api/exception/SelectorValueException.py
@@ -2,6 +2,9 @@ from http.client import HTTPException
 
 
 class SelectorValueException(HTTPException):
-    def __init__(self):
-        super().__init__("언론사 데이터의 셀렉터나 Base Url이 누락됐습니다")
+    def __init__(self, message : str | None = None):
         self.status_code = 404
+        if message:
+            super().__init__(message)
+        else:
+            super().__init__("언론사 데이터의 셀렉터나 Base Url이 누락됐습니다")
