e1ae31379460fa2e8ce024406e5dc32bcdac1f6f
lhetl <hyun91000@gmail.com>
2026-07-17 02:21:54 +0900

    :recycle: schemas 필드 수정

--- src/crawler_api/schemas/Article.py
+++ src/crawler_api/schemas/Article.py
@@ -4,30 +4,27 @@ from beanie import PydanticObjectId
 from pydantic import BaseModel, ConfigDict
 
 
-class ArticleBase(BaseModel):
-    url : str
-    title : str
-    content : str
-    company_name : str
-    reporter : str | None = None
-    category : str | None = None
-    img_list : list[str] | None = None
-    published_at : str | None = None
+class ArticleCreate(BaseModel):
+    url: str
+    title: str
+    content: str
+    company_name: str
+    reporter: str | None = None
+    category: str | None = None
+    img_list: list[str] | None = None
+    published_at: datetime | None = None
 
-class ArticleCreate(ArticleBase):
-    pass
-
-class ArticleUpdate(ArticleBase):
+class ArticleUpdate(BaseModel):
     title: str | None = None
     content: str | None = None
     company_name: str | None = None
     reporter: str | None = None
     category: str | None = None
     img_list: list[str] | None = None
-    published_at: str | None = None
+    published_at: datetime | None = None
 
-class ArticleRead(ArticleBase):
-    model_config = ConfigDict(from_attributes=True, populate_by_name=True)
+class ArticleRead(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
     id: PydanticObjectId
     title: str
     company_name: str
