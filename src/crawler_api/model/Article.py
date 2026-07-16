560d17b6892cb778dbf82e5f7d94a2afa4f060dc
lhetl <hyun91000@gmail.com>
2026-07-17 02:21:36 +0900

    :sparkles: updated_at 필드 추가

--- src/crawler_api/model/Article.py
+++ src/crawler_api/model/Article.py
@@ -1,13 +1,14 @@
 from datetime import timezone, datetime
+from typing import Annotated
 
 import pymongo
 from pydantic import Field
 from beanie import Document, Indexed
 
 
-class article(Document):
+class Article(Document):
     # 중복 방지용
-    url : Indexed(str, unique=True)
+    url : Annotated[str, Indexed(unique=True)]
 
     title : str
     content : str
@@ -17,9 +18,10 @@ class article(Document):
     img_list : list[str] | None = None
     published_at : datetime | None = None
     crawled_at : datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at : datetime | None = None
 
     class Settings:
         name = "article"
         indexes = [
-            [("published_at",pymongo.DESCENDING)]
+            [("published_at",pymongo.DESCENDING), ("crawled_at", pymongo.ASCENDING)]
         ]
