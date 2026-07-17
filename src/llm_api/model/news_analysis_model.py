a0ea6f4dbce3aee964cd5a03918e97b58ac91971
Neun99 <eoo1010@naver.com>
2026-07-17 17:28:56 +0900

    :memo: TODO 추가

--- src/llm_api/model/news_analysis_model.py
+++ src/llm_api/model/news_analysis_model.py
@@ -11,5 +11,5 @@ class NewsAnalysisModel(Base):
     category: Mapped[str] = mapped_column(String(10)) #카테고리. 10글자는 안넘어가겠지..?
     topic_id: Mapped[int] = mapped_column(ForeignKey("topic.id")) #주제 id
     score: Mapped[float] #신뢰점수
-    score_detail: Mapped[dict | None] = mapped_column(JSON) #디버깅용, 신뢰점수 구하는 과정 저장
+    score_detail: Mapped[dict | None] = mapped_column(JSON) #디버깅용, 신뢰점수 구하는 과정 저장 #TODO:기능구현 후 Nullable 제거하기
     analyzed_at: Mapped[datetime] = mapped_column(default = datetime.now) #분석한 시간
