b1ce8937401859fe76300af7264529fca5388629
Neun99 <eoo1010@naver.com>
2026-07-17 17:27:19 +0900

    :recycle: 변수명 변경

--- src/llm_api/model/keyword_model.py
+++ src/llm_api/model/keyword_model.py
@@ -5,5 +5,5 @@ from sqlalchemy import String, JSON
 class KeywordModel(Base):
     __tablename__ = "keyword"
     id: Mapped[int] = mapped_column(primary_key = True)
-    keyword_text: Mapped[str] = mapped_column(String(50), unique = True) #키워드
-    embedding: Mapped[list] = mapped_column(JSON) #키워드의 임베딩 값. 비교하면서 비슷한거면 병합시킴
+    keyword: Mapped[str] = mapped_column(String(50), unique = True) #키워드
+    embedding: Mapped[list | None] = mapped_column(JSON) #키워드의 임베딩 값. 비교하면서 비슷한거면 병합시킴
