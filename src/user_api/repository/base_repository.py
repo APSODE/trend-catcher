fd6116602eb11513e62714f777059e69cb046b62
APSODE <2gunbo74@gmail.com>
2026-07-18 18:19:52 +0900

    :sparkles: 각 리포지토리에서 상속받아 사용하는 기반 repository

---
+++ src/user_api/repository/base_repository.py
@@ -0,0 +1,44 @@
+from typing import Generic, List, Optional, Type, TypeVar
+
+from sqlalchemy.sql.elements import ColumnElement
+
+from src.user_api.db.db_controller import DatabaseController
+from src.user_api.model.base_model import BaseModel
+
+ModelType = TypeVar("ModelType", bound = BaseModel)
+
+
+class BaseRepository(Generic[ModelType]):
+    def __init__(self, db_controller: DatabaseController, model_class: ModelType | Type[ModelType]):
+        self._db_controller = db_controller
+        self._model_class = model_class
+
+    @property
+    def model_class(self) -> ModelType | Type[ModelType]:
+        return self._model_class
+
+    @property
+    def db_controller(self) -> DatabaseController:
+        return self._db_controller
+
+
+    async def add_data(self, new_data: ModelType | List[ModelType],with_commit: bool = False) -> None:
+        await self.db_controller.add(new_data, with_commit)
+
+    async def find(self, filter: Optional[ColumnElement[bool]] = None, amount: int = 0) -> List[ModelType]:
+        return await self._db_controller.get(self._model_class, filter = filter, amount = amount)
+
+    async def find_one(self, filter: ColumnElement[bool]) -> Optional[ModelType]:
+        results = await self.find(filter, amount = 1)
+        return results[0] if results else None
+
+    async def get_by_id(self, target_id: int) -> Optional[ModelType]:
+        return await self.find_one(filter = self._model_class.id == target_id)
+
+    async def get_all(self) -> List[ModelType]:
+        return await self._db_controller.get(self._model_class, amount = 0)
+
+    async def is_exist(self, filter: ColumnElement[bool]) -> bool:
+        results = await self._db_controller.get(self._model_class, filter = filter, amount = 1)
+        return len(results) > 0
+
