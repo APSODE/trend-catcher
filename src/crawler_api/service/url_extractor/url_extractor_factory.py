feea3fe2904850531e9377d14ab85371978b1c6e
lhetl <hyun91000@gmail.com>
2026-07-19 21:31:55 +0900

    :recycle: factory 필터 수정

--- src/crawler_api/service/url_extractor/url_extractor_factory.py
+++ src/crawler_api/service/url_extractor/url_extractor_factory.py
@@ -1,5 +1,4 @@
 from src.crawler_api.constant.news_sitemap import NewsSitemap, SitemapType
-from src.crawler_api.exception import selector_value_exception
 from src.crawler_api.service.url_extractor.page_extractor import PageExtractor
 from src.crawler_api.service.url_extractor.xml_extractor import XMLExtractor
 
@@ -9,7 +8,4 @@ class UrlExtractorFactory:
     def create(source : NewsSitemap):
         if source.value.sitemap_type == SitemapType.XML:
             return XMLExtractor()
-        else:
-            if source.value.selector is None:
-                raise selector_value_exception
-            return PageExtractor()
+        return PageExtractor()
