3af3e9794e0906526a983155c31375d81a78a40d
Neun99 <eoo1010@naver.com>
2026-07-26 01:20:03 +0900

    :sparkles: 키워드로 뉴스, 뉴스로 키워드 검색기능 추가

--- src/llm_api/repository/news_keyword_map_repository.py
+++ src/llm_api/repository/news_keyword_map_repository.py
@@ -1,5 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from model.news_keyword_map_model import NewsKeywordMapModel
+from src.llm_api.model.news_keyword_map_model import NewsKeywordMapModel
+from src.llm_api.model.keyword_model import KeywordModel
+from sqlalchemy import select
 
 class NewsKeywordMapRepository:
     #새 매핑 추가
@@ -10,3 +12,15 @@ class NewsKeywordMapRepository:
         )
         session.add(mapping)
         await session.flush()
+
+    #뉴스로 키워드 탐색
+    async def get_keywords_by_news(self, session:AsyncSession, news_id: int) -> list[KeywordModel]:
+        query = select(KeywordModel).join(NewsKeywordMapModel, NewsKeywordMapModel.keyword_id == KeywordModel.id).where(NewsKeywordMapModel.news_id == news_id)
+        result = await session.execute(query)
+        return list(result.scalars().all())
+
+    #키워드로 뉴스 탐색
+    async def get_news_by_keyword(self, session:AsyncSession, keyword_id: int) -> list[int]:
+        query = select(NewsKeywordMapModel.news_id).where(NewsKeywordMapModel.keyword_id == keyword_id)
+        result = await session.execute(query)
+        return list(result.scalars().all())
