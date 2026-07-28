b7d1c7a08985e4f7fbbafaf9787f2499dfd6a0dd
Neun99 <eoo1010@naver.com>
2026-07-28 18:24:32 +0900

    :recycle: db 비동기화

--- src/llm_api/db.py
+++ src/llm_api/db.py
@@ -1,4 +1,4 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 engine = create_async_engine("sqlite+aiosqlite:///test.db")  # TODO: 오라클로 교체 필요
-SessionLocal = async_sessionmaker(bind=engine)
+SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
