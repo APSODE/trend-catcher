0c73f3d2f89fa54e0116f11daa2eb3fc1f84da7f
Neun99 <eoo1010@naver.com>
2026-07-28 18:22:45 +0900

    :bug: 오타 수정

--- src/llm_api/model/news_topic_map_model.py
+++ src/llm_api/model/news_topic_map_model.py
@@ -1,9 +1,9 @@
-from sqlalchemy import ForeignKey, Float
+from sqlalchemy import ForeignKey
 from src.llm_api.model.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
 
 class NewsTopicMapModel(Base):
-    __talename__ = "news_topic_map"
+    __tablename__ = "news_topic_map"
 
     news_id: Mapped[int] = mapped_column(ForeignKey("news_analysis.id"), primary_key = True)
     topic_id: Mapped[int] = mapped_column(ForeignKey("topic.id"), primary_key = True)
