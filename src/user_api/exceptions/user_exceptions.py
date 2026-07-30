71a54bff5cdd0b3b0943270d0d2271b47ebd908e
APSODE <2gunbo74@gmail.com>
2026-07-30 16:00:55 +0900

    :sparkles: App 단위의 BaseException 정의 및 상속을 통한 세분화

--- src/user_api/exceptions/user_exceptions.py
+++
@@ -1,6 +0,0 @@
-class InvalidNameException(Exception):
-    def __init__(self, name: str, max_length: int):
-        super().__init__(f"Invalid name length. Expected {max_length} characters, but received {len(name)}.")
-        
-        
-        
