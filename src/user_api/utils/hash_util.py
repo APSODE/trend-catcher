efce39f30c8c08d02b8e3200da43284c6897f6e1
APSODE <2gunbo74@gmail.com>
2026-07-30 15:57:38 +0900

    :recycle: Util 클래스로 계층 강등 및 네이밍 추상화

--- src/user_api/utils/hash_util.py
+++ src/user_api/utils/hash_util.py
@@ -1,13 +1,14 @@
+from typing import NewType
 from hashlib import sha256
 from os import urandom
-
-from src.user_api.constant.user_model_constant import MIN_SALT_LENGTH, MAX_SALT_LENGTH
+from src.user_api.constant.account_constant import MIN_SALT_LENGTH, MAX_SALT_LENGTH
 from src.user_api.exceptions.hash_exceptions import IllegalSaltException
 
+HashedString = NewType("HashedString", str)
 
-class StringHashService:
+class HashUtil:
     @staticmethod
-    async def create_salt(length: int) -> str:
+    def create_salt(length: int) -> str:
         if MIN_SALT_LENGTH <= length <= MAX_SALT_LENGTH:
             return sha256(urandom(length)).hexdigest()
 
@@ -17,7 +18,9 @@ class StringHashService:
 
 
     @staticmethod
-    async def get_hashed_password(raw_password: str, salt: str) -> str:
+    def get_hashed_string(target: str, salt: str) -> HashedString:
+        return HashedString(sha256(f"{target}{salt}".encode()).hexdigest())
+
 
 
 
