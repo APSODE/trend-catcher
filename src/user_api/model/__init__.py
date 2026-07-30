7826b54422fe6ef77653df600405c5fd58132cfa
APSODE <2gunbo74@gmail.com>
2026-07-30 15:53:48 +0900

    :sparkles: Model 초기화 과정 수행을 위한 __init__.py 기재

---
+++ src/user_api/model/__init__.py
@@ -0,0 +1,13 @@
+from src.user_api.model.base_model import BaseModel
+from src.user_api.model.user_model import UserModel
+from src.user_api.model.account_model import AccountModel
+from src.user_api.model.category_model import CategoryModel
+from src.user_api.model.user_category_model import UserCategoryModel
+
+__all__ = [
+    "BaseModel",
+    "UserModel",
+    "AccountModel",
+    "CategoryModel",
+    "UserCategoryModel",
+]
