a4fe89940f0609b88c21144b76e5625eb0e29b82
APSODE <2gunbo74@gmail.com>
2026-07-30 16:14:02 +0900

    :sparkles: Context Manager 사용 도입

--- src/user_api/router/user_router.py
+++ src/user_api/router/user_router.py
@@ -1,14 +1,37 @@
-from abc import ABC
-from typing import List, Dict
+from typing import AsyncGenerator
 
+from fastapi import Depends
 from starlette.requests import Request
 
-from src.user_api.db.db_controller import DatabaseController
+from src.user_api.db.db_creator import DatabaseCreator
+from src.user_api.db.user_account_context import UserAccountContext
+from src.user_api.dto.request_data import RegisterRequest, LoginRequest, DeleteRequest
+from src.user_api.repository.user_repository import UserRepository
+from src.user_api.repository.account_repository import AccountRepository
 from src.user_api.router.base_router import BaseRouter
+from src.user_api.service.user_account_service import UserAccountService
 
 
-class UserRouter(BaseRouter):
-    def __init__(self, db_controller: DatabaseController):
+async def _get_user_account_context() -> AsyncGenerator[UserAccountContext, None]:
+    context = UserAccountContext(
+        session_factory = DatabaseCreator().session,
+        repository_factories = {
+            UserRepository: UserRepository,
+            AccountRepository: AccountRepository,
+        }
+    )
+
+    async with context:
+        yield context
+
+
+async def _get_user_account_service(context: UserAccountContext = Depends(_get_user_account_context)) -> UserAccountService:
+    return UserAccountService(
+        account_repository = context.accounts,
+        user_repository = context.users,
+    )
+
+
 class UserRouter(BaseRouter[UserAccountService]):
     def __init__(self):
         super().__init__(
@@ -18,23 +41,17 @@ class UserRouter(BaseRouter[UserAccountService]):
             get_service = _get_user_account_service,
         )
 
-    @staticmethod
-    def create_router(db_controller: DatabaseController):
-        return UserRouter(db_controller = db_controller)
-
     def setup_routes(self):
-        @self.get("/login")
-        async def login(request: Request):
-            pass
+        @self.post("/register")
+        async def register(request: RegisterRequest, service: UserAccountService = Depends(self._get_service)):
+            await service.register(request)
+            return {"message": "success"}
 
-        @self.get("/register")
-        async def register(request: Request):
-            pass
+        @self.post("/login")
+        async def login(request: LoginRequest, service: UserAccountService = Depends(self._get_service)):
+            is_success = await service.login(request)
+            return {"success": is_success}
 
-        @self.get("/query-interest-category")
-        async def query_interest_category(request: Request):
-            pass
-
-        @self.get("/change-permission")
-        async def change_permission(request: Request):
-            pass
+        @self.post("/delete-user")
+        async def delete(request: DeleteRequest, service: UserAccountService = Depends(self._get_service)):
+            await service.delete(request)
