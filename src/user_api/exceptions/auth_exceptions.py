561741c8818f40964ed13aa473e0c32cb0b9b5f0
APSODE <2gunbo74@gmail.com>
2026-08-03 20:05:57 +0900

    :sparkles: JWT Exception & Handler

---
+++ src/user_api/exceptions/auth_exceptions.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+
+from src.user_api.exceptions.app_exception import AppException
+
+
+class InvalidToken(AppException):
+    status_code = HTTPStatus.UNAUTHORIZED
+
+    def __init__(self):
+        self.message = "Invalid token data"
+
+class ExpiredToken(AppException):
+    status_code = HTTPStatus.UNAUTHORIZED
+
+    def __init__(self):
+        super().__init__("Current token is expired")
+
+
+class MismatchTokenType(AppException):
+    status_code = HTTPStatus.UNAUTHORIZED
+
+    def __init__(self):
+        super().__init__("Invalid token type")
