62d152a92b91fc10ecb66e342a24b233f673a2fd
APSODE <2gunbo74@gmail.com>
2026-08-04 23:44:11 +0900

    :sparkles: 해시태그 기능 구현을 위한 Request DTO 추가

---
+++ src/user_api/dto/hashtag_data.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+
+class HashtagData(BaseModel):
+    name: str
--- src/user_api/dto/request_data.py
+++ src/user_api/dto/request_data.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 
 from src.user_api.constant.permission import Permission
 from src.user_api.dto.account_data import AccountData
+from src.user_api.dto.hashtag_data import HashtagData
 from src.user_api.dto.user_data import UserData
 
 
@@ -22,6 +23,27 @@ class LoginRequest(BaseModel):
 class DeleteRequest(BaseModel):
     login_id: str
 
-
 class RefreshRequest(BaseModel):
     refresh_token: str
+
+class FollowHashtagRequest(BaseModel):
+    target_user_pk: int
+    target_hashtag: HashtagData
+
+class UnfollowHashtagRequest(BaseModel):
+    target_user_pk: int
+    target_hashtag: HashtagData
+
+# # ==== 사용..? 안할수도...?
+# class CreateHashtagRequest(HashtagData):
+#     pass # 이후 필요시 해싱값 추가 예정
+#
+# class SearchHashtagRequest(BaseModel):
+#     target_name: str
+#
+# class UpdateHashtagRequest(BaseModel):
+#     target_name: str
+#     new_data: HashtagData
+#
+# class DeleteHashtagRequest(BaseModel):
+#     target_name: str
