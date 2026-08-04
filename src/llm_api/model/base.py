1595aa3a3531440a2186f0fb204f4abadfe7c5bf
Neun99 <eoo1010@naver.com>
2026-08-04 21:17:06 +0900

    :recycle: id 네이밍 pk로 수정

--- src/llm_api/model/base.py
+++ src/llm_api/model/base.py
@@ -4,7 +4,7 @@ from sqlalchemy import MetaData, Identity
 #디버깅이나 제약조건 관리 등에서 쉽게 확인할 수 있게 이름에 적용시킬 컨벤션을 정의, 모든 모델에 통일적용
 NAMING_CONVENTION = {
     "pk": "pk_%(table_name)s", #기본키, pk_테이블명
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s", #외래키, fk_테이블명_컬럼명_부모테이블명
+    "fk": "fk_%(table_name)s_%(column_0_name)s", #외래키, fk_테이블명_컬럼명
     "ix": "idx_%(table_name)s_%(column_0_name)s", #인덱스, idx_테이블명_컬럼명
     "uq": "uq_%(table_name)s_%(column_0_name)s", #유일성, uq_테이블명_컬럼명
     "ck": "ck_%(table_name)s_%(constraint_name)s" #제약성 체크, ck_테이블명_제약명
@@ -13,4 +13,4 @@ NAMING_CONVENTION = {
 class AbstractBaseModel(DeclarativeBase):
     __abstract__ = True #이거 실체 없음
     metadata = MetaData(naming_convention = NAMING_CONVENTION) #컨벤션 적용
-    id: Mapped[int] = mapped_column(Identity(start = 1, increment = 1),primary_key = True) #기본키
+    pk: Mapped[int] = mapped_column(Identity(start = 1, increment = 1),primary_key = True) #기본키
