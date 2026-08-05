8f18344b5f7e3239c352a634c0a33de4389ddaa7
lhetl <hyun91000@gmail.com>
2026-08-05 19:05:51 +0900

    :sparkles: context 설정

--- src/crawler_api/dependencies/article.py
+++ src/crawler_api/dependencies/article.py
@@ -1,7 +1,13 @@
-from src.crawler_api.repository.article_repository import ArticleRepository
+from typing import AsyncGenerator
+
+from fastapi import Depends, Request
+
+from src.crawler_api.db.article_context import ArticleContext
 from src.crawler_api.service.article_service import ArticleService
 
+async def get_article_context(request : Request) -> AsyncGenerator[ArticleContext, None]:
+    async with ArticleContext(request.app.state.mongo_client) as ctx:
+        yield ctx
 
-def get_article_service() -> ArticleService:
-    repository = ArticleRepository()
-    return ArticleService(repository)
+async def get_article_service(ctx : ArticleContext = Depends(get_article_context)) -> ArticleService:
+    return ArticleService(article_repository = ctx.articles)
