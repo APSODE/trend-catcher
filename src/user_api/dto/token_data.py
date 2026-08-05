88c5b406312949a115e3376e6fb3919828859116
APSODE <2gunbo74@gmail.com>
2026-08-06 00:20:00 +0900

    :sparkles: JWT Token DTO 정의

--- src/user_api/dto/token_data.py
+++ src/user_api/dto/token_data.py
@@ -1,7 +1,35 @@
-from pydantic import BaseModel
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseModel, field_validator
+
+
+class TokenType(Enum):
+    ACCESS = "access"
+    REFRESH = "refresh"
 
 
 class TokenPair(BaseModel):
     access_token: str
     refresh_token: str
     token_type: str = "bearer"
+
+class JsonWebToken(BaseModel):
+    jwt_id: str
+    account_pk: int
+    type: TokenType
+    iat: datetime
+    exp: datetime
+    session_id: str
+
+    @field_validator("account_pk", mode = "before")
+    @classmethod
+    def parse_account_pk(cls, value):
+        return int(value)
+
+class RefreshJWT(BaseModel):
+    access_token_id: str
+
+
+
+
