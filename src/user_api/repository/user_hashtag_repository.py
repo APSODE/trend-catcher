83ee2163578e09cb30a3ded73973e0436f446d50
APSODE <2gunbo74@gmail.com>
2026-08-06 01:43:21 +0900

    :lipstick: 잘못된 파라미터명 수정

--- src/user_api/repository/user_hashtag_repository.py
+++ src/user_api/repository/user_hashtag_repository.py
@@ -8,18 +8,18 @@ class UserHashtagRepository(BaseRepository[UserHashtagModel]):
         super().__init__(db_controller, UserHashtagModel)
 
     async def create_relation(self, user_pk: int, hashtag_pk: int, with_flush: bool = False) -> UserHashtagModel:
-        new_relation = UserHashtagModel(user_pk = user_pk, hashtag_pk = hashtag_pk)
+        new_relation = UserHashtagModel(user_fk = user_pk, hashtag_fk = hashtag_pk)
         await self.add_data(new_relation, with_flush = with_flush)
         return new_relation
 
     async def delete_relation(self, user_pk: int, hashtag_pk: int, with_flush: bool = False) -> None:
         await self.delete(
-            filter = (self.model_class.user_pk == user_pk) & (self.model_class.hashtag_pk == hashtag_pk),
+            filter = (self.model_class.user_fk == user_pk) & (self.model_class.hashtag_fk == hashtag_pk),
             with_flush = with_flush,
         )
 
     async def is_exist_relation(self, user_pk: int, hashtag_pk: int) -> bool:
         return await self.is_exist(
-            filter = (self.model_class.user_pk == user_pk) & (self.model_class.hashtag_pk == hashtag_pk)
+            filter = (self.model_class.user_fk == user_pk) & (self.model_class.hashtag_fk == hashtag_pk)
         )
 
