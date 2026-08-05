b9a5718a054c1fd2e8cf6c71e960ca9af3c9bbeb
lhetl <hyun91000@gmail.com>
2026-08-05 20:30:57 +0900

    :sparkles: lifespan event publisher 추가

--- src/crawler_api/main.py
+++ src/crawler_api/main.py
@@ -1,15 +1,39 @@
-from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
-from src.crawler_api.db.database_session import lifespan
+from fastapi import FastAPI
+
+from src.crawler_api.config.scheduler import init_scheduler
+from src.crawler_api.db.database_session import init_db
+from src.crawler_api.event.event_publisher import EventPublisher
 from src.crawler_api.route import article_router
 
-app = FastAPI(lifespan = lifespan)
-app.add_middleware(
-    CORSMiddleware,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #DB 실행
+    client = await init_db()
+    app.state.mongo_client = client
+
+    #event publisher
+    event_publisher = EventPublisher()
+    #event_publisher.subscribe()
+    app.state.event_publisher = event_publisher
+
+    #스케줄러
+    scheduler = init_scheduler(app)
+    scheduler.start()
+    app.state.scheduler = scheduler
+
+    yield
+
+    scheduler.shutdown()
+    await client.close()
+
+
+CrawlerAPI = FastAPI(
+    title = "Crawler API",
+    lifespan = lifespan,
+    description = "Crawler API",
 )
 
-app.include_router(article_router.router)
+CrawlerAPI.include_router(article_router.router)
