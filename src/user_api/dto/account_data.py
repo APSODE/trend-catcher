7297df7b1a0de6c795699e4603654d1f46e0a638
APSODE <2gunbo74@gmail.com>
2026-08-06 03:45:15 +0900

    :recycle: Account DTO 보안성 향상을 위한 구조 변경

--- src/user_api/dto/account_data.py
+++ src/user_api/dto/account_data.py
@@ -1,8 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class AccountData(BaseModel):
+    model_config = ConfigDict(from_attributes = True)
+
+    pk: int
+    user_fk: int
     login_id: str
-    password: str
-    salt: str
+
 
