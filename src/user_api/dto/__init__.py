ea94ee4465d32311af81fc1b00f0c4e4555a2e5f
APSODE <2gunbo74@gmail.com>
2026-08-06 04:00:31 +0900

    :heavy_plus_sign: serialize_many도 접근가능하도록 추가

--- src/user_api/dto/__init__.py
+++ src/user_api/dto/__init__.py
@@ -1,7 +1,9 @@
 from src.user_api.dto.model_serializer import ModelSerializer as _serializer
 
 serialize = _serializer.serialize
+serialize_many = _serializer.serialize_many
 
 __all__ = [
-    "serialize"
+    "serialize",
+    "serialize_many"
 ]
