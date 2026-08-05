9a4ecd05ba7a4bf76464b0449cfa763afef0c12d
APSODE <2gunbo74@gmail.com>
2026-08-06 03:45:50 +0900

    :recycle: serialize를 위한 기반 기능 적용

--- src/user_api/dto/user_data.py
+++ src/user_api/dto/user_data.py
@@ -1,10 +1,12 @@
 from typing import List
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from src.user_api.constant.permission import Permission
 from src.user_api.dto.account_data import AccountData
 
 
 class UserData(BaseModel):
+    model_config = ConfigDict(from_attributes = True)
+
     user_account: AccountData
     name: str
     permission: int | Permission
