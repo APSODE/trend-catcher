125122600c158172c82e1e4769418ae257d03b0a
APSODE <2gunbo74@gmail.com>
2026-08-06 01:46:32 +0900

    :sparkles: JWT 주입 및 logout 엔드포인트 추가

--- src/user_api/router/user_router.py
+++ src/user_api/router/user_router.py
@@ -1,11 +1,13 @@
-from fastapi import Depends
+from fastapi import Depends, Security
+from fastapi.security import HTTPAuthorizationCredentials
 
+from src.user_api.auth.denendencies import get_current_account_pk, bearer_scheme
 from src.user_api.dto.request_data import (
     RegisterRequest,
     LoginRequest,
     DeleteRequest,
     RefreshRequest,
-    FollowHashtagRequest, UnfollowHashtagRequest
+    FollowHashtagRequest, UnfollowHashtagRequest, LogoutRequest
 )
 from src.user_api.dto.token_data import TokenPair
 from src.user_api.router.base_router import BaseRouter
@@ -17,7 +19,7 @@ class UserRouter(BaseRouter):
     def __init__(self):
         super().__init__(
             prefix = "/user",
-            tags = ["dev", "inner-connection-only"],
+            tags = ["dev"],
             response = {404: {"description": "Not Found"}}
         )
 
@@ -31,6 +33,11 @@ class UserRouter(BaseRouter):
         async def login(request: LoginRequest, service: UserAccountService = Depends(get_user_account_service)):
             return await service.login(request)
 
+        @self.post("/logout")
+        async def logout(credentials: HTTPAuthorizationCredentials = Security(bearer_scheme), service: UserAccountService = Depends(get_user_account_service)):
+            await service.logout(credentials.credentials)
+            return {"message": "success"}
+
         @self.delete("/delete-user")
         async def delete(request: DeleteRequest, service: UserAccountService = Depends(get_user_account_service)):
             await service.delete(request)
@@ -40,9 +47,13 @@ class UserRouter(BaseRouter):
             return await service.refresh_token(request.refresh_token)
 
         @self.post("/follow-hashtag")
-        async def follow_hashtag(request: FollowHashtagRequest, service: UserHashtagService = Depends(get_user_hashtag_service)):
-            await service.follow_hashtag(request)
+        async def follow_hashtag(request: FollowHashtagRequest,
+                                 user_pk: int = Depends(get_current_account_pk),
+                                 service: UserHashtagService = Depends(get_user_hashtag_service)):
+            await service.follow_hashtag(request, user_pk)
 
         @self.post("/unfollow-hashtag")
-        async def unfollow_hashtag(request: UnfollowHashtagRequest, service: UserHashtagService = Depends(get_user_hashtag_service)):
-            await service.unfollow_hashtag(request)
+        async def unfollow_hashtag(request: UnfollowHashtagRequest,
+                                   user_pk: int = Depends(get_current_account_pk),
+                                   service: UserHashtagService = Depends(get_user_hashtag_service)):
+            await service.unfollow_hashtag(request, user_pk)
