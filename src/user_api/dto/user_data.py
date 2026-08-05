32b244bf674db73772e911510ee6312863cb8710
APSODE <2gunbo74@gmail.com>
2026-08-06 05:01:44 +0900

    :sparkles: DTO serialize를 위한 바인딩

--- src/user_api/dto/user_data.py
+++ src/user_api/dto/user_data.py
@@ -1,9 +1,12 @@
 from typing import List
 from pydantic import BaseModel, ConfigDict
 from src.user_api.constant.permission import Permission
+from src.user_api.decorator import bind_model
 from src.user_api.dto.account_data import AccountData
+from src.user_api.model import UserModel
 
 
+@bind_model(UserModel)
 class UserData(BaseModel):
     model_config = ConfigDict(from_attributes = True)
 
