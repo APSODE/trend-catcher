f90bdb74440596366726546d45ef3d441b7d6fbb
lhetl <hyun91000@gmail.com>
2026-08-06 21:56:31 +0900

    :bug: 상태코드 기본값 수정

--- src/crawler_api/exception/base_error_exception.py
+++ src/crawler_api/exception/base_error_exception.py
@@ -2,7 +2,6 @@ from fastapi import HTTPException
 
 class BaseErrorException(HTTPException):
     def __init__(self, message : str | None = None, status_code : int = 404):
-        self.status_code = 404
         if message:
             super().__init__(detail = message, status_code = status_code)
         else:
