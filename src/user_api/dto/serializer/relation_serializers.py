64539cc3a63caa999d0d131af9c730f1ddda88ca
APSODE <2gunbo74@gmail.com>
2026-08-06 23:37:59 +0900

    :memo: deprecated 기록 추가

--- src/user_api/dto/serializer/relation_serializers.py
+++ src/user_api/dto/serializer/relation_serializers.py
@@ -1,14 +1,15 @@
 from typing import List
+from warnings import deprecated
 
 from src.user_api.dto.account_data import AccountData
 from src.user_api.dto.hashtag_data import HashtagData
 from src.user_api.dto.serializer import serialize_many
 from src.user_api.model import UserModel
 
-
+@deprecated("relation 데코레이터로 변경되었음")
 def _serialize_accounts(user: UserModel) -> List[AccountData]:
     return serialize_many(user.accounts, AccountData)
 
-
+@deprecated("relation 데코레이터로 변경되었음")
 def _serialize_interest(user: UserModel) -> List[HashtagData]:
     return serialize_many([relation.hashtag_model for relation in user.interest], HashtagData)
