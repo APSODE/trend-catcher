e62280a99ddc52a235db2ae1851b0363e55e1cb7
APSODE <2gunbo74@gmail.com>
2026-08-06 23:01:42 +0900

    :white_check_mark: 테스트를 위한 라우터 등록

--- src/user_api/router/internal/__init__.py
+++ src/user_api/router/internal/__init__.py
@@ -1,12 +1,14 @@
 from typing import List
 
 from src.user_api.router.base_router import BaseRouter
+from src.user_api.router.internal.account_router import AccountRouter
 from src.user_api.router.internal.user_router import UserRouter
 from src.user_api.router.internal.hashtag_router import HashtagRouter
 
 INTERNAL_ROUTERS: List[BaseRouter] = [
     UserRouter(),
-    HashtagRouter()
+    HashtagRouter(),
+    AccountRouter()
 ]
 
 __all__ = ["INTERNAL_ROUTERS"]
