c97107bf7fa36f3740dd28ddf3a404a8f6ba2aa7
Neun99 <eoo1010@naver.com>
2026-08-07 19:12:24 +0900

    :fire:

--- src/llm_api/db.py
+++
@@ -1,4 +0,0 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
-engine = create_async_engine("sqlite+aiosqlite:///test.db")  # TODO: 오라클로 교체 필요
-SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
