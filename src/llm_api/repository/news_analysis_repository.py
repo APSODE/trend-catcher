dc3f2f2515a6e7ba78eb7e73c4432c17decd4959
Neun99 <eoo1010@naver.com>
2026-08-13 22:55:54 +0900

    :recycle: 알림용 메소드 추가, 안 쓰는 메소드 제거

--- src/llm_api/repository/news_analysis_repository.py
+++ src/llm_api/repository/news_analysis_repository.py
@@ -1,7 +1,7 @@
 from src.llm_api.repository.base_repository import BaseRepository
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.llm_api.model.news_analysis_model import NewsAnalysisModel
-
+from datetime import datetime
 
 class NewsAnalysisRepository(BaseRepository[NewsAnalysisModel]):
     def __init__(self, session: AsyncSession):
@@ -11,10 +11,6 @@ class NewsAnalysisRepository(BaseRepository[NewsAnalysisModel]):
     async def is_exist_by_crawled_id(self, crawled_id: str) -> bool:
         result = await self._find_one(NewsAnalysisModel.crawled_id == crawled_id)
         return result is not None
-
-    #크롤러id로 검색
-    async def find_by_crawled_id(self, crawled_id: str) -> NewsAnalysisModel | None:
-        return await self._find_one(NewsAnalysisModel.crawled_id == crawled_id)
 
     #점수 없는애들 검색
     async def find_unscored(self, limit: int) -> list[NewsAnalysisModel]:
@@ -33,4 +29,12 @@ class NewsAnalysisRepository(BaseRepository[NewsAnalysisModel]):
         new_analysis = NewsAnalysisModel(crawled_id = crawled_id, topic_fk = topic_fk, content_score = content_score)
         return await self.save(new_analysis)
 
-    #TODO: 알림용 이번 타임 뉴스만 반환 메소드 필요
+    # 시간 이후 점수 있는 뉴스 리턴
+    async def find_scored_since(self, since: datetime) -> list[NewsAnalysisModel]:
+        return await self._find_all((NewsAnalysisModel.analyzed_at >= since) & (NewsAnalysisModel.score.is_not(None)))
+
+    #pk 목록으로 조회
+    async def find_by_pks(self, pks: list[int]) -> list[NewsAnalysisModel]:
+        if not pks:
+            return []
+        return await self._find_all(NewsAnalysisModel.pk.in_(pks))
