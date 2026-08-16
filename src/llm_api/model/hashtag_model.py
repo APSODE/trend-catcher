52f399273a7cc6739971cd626ed59ead8e2a9f08
Neun99 <eoo1010@naver.com>
2026-08-16 18:29:48 +0900

    :bug: 오라클이 json 못받아먹는거 해결

--- src/llm_api/model/hashtag_model.py
+++ src/llm_api/model/hashtag_model.py
@@ -1,11 +1,12 @@
-from src.llm_api.model.base_model import AbstractBaseModel
+from src.llm_api.model.base_model import AbstractBaseModel, JsonType
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, JSON
+from sqlalchemy import String
+
 
 class HashtagModel(AbstractBaseModel):
     __tablename__ = "hashtag"
 
     hashtag: Mapped[str] = mapped_column(String(32), unique= True) #해시태그 내용
-    aliases: Mapped[list[str]] = mapped_column(JSON) #동의어
-    children: Mapped[list[str]] = mapped_column(JSON) #하위 개체
-    embedding: Mapped[list[float]] = mapped_column(JSON) #임베딩
+    aliases: Mapped[list[str]] = mapped_column(JsonType) #동의어
+    children: Mapped[list[str]] = mapped_column(JsonType) #하위 개체
+    embedding: Mapped[list[float]] = mapped_column(JsonType) #임베딩
--- src/llm_api/model/keyword_model.py
+++ src/llm_api/model/keyword_model.py
@@ -1,9 +1,9 @@
-from src.llm_api.model.base_model import AbstractBaseModel
+from src.llm_api.model.base_model import AbstractBaseModel, JsonType
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, JSON
+from sqlalchemy import String
 
 class KeywordModel(AbstractBaseModel):
     __tablename__ = "keyword"
 
     keyword: Mapped[str] = mapped_column(String(50), unique = True) #키워드
-    embedding: Mapped[list[float]] = mapped_column(JSON) #키워드의 임베딩 값. 비교하면서 비슷한거면 병합시킴
+    embedding: Mapped[list[float]] = mapped_column(JsonType) #키워드의 임베딩 값. 비교하면서 비슷한거면 병합시킴
