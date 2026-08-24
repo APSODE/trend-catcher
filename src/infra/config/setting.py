5d1fe40c633130a3ca90f956948645711ef6e291
lhetl <hyun91000@gmail.com>
2026-08-25 01:56:24 +0900

    :recycle: jwt 필드 삭제

--- src/infra/config/setting.py
+++ src/infra/config/setting.py
@@ -3,15 +3,13 @@ from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
-#파일 구조 변경시 수정 필요
-ENV_PATH = Path(__file__).resolve().parents[1] / "url.env"
 
+ENV_PATH = Path(__file__).resolve().parents[1] / "url.env"
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=str(ENV_PATH),
         env_file_encoding="utf-8",
-        #env에 선언 안된 값 존재시 무시
         extra="ignore",
     )
 
@@ -22,9 +20,7 @@ class Settings(BaseSettings):
     crawler_api_url : str = Field(validation_alias="CRAWLER_API_URL")
     llm_api_url : str = Field(validation_alias="LLM_API_URL")
 
-    jwt_secret_key: str = Field(validation_alias="JWT_SECRET_KEY")
-    jwt_algorithm: str = Field(default="HS256", validation_alias="JWT_ALGORITHM")
-    redis_url: str = Field(default="redis://redis:6379", validation_alias="REDIS_URL")
+    frontend_origins: str = Field(validation_alias="FRONTEND_ORIGINS")
 
     default_timeout : float = 10.0
     llm_api_timeout : float = 30.0
@@ -32,9 +28,3 @@ class Settings(BaseSettings):
 @lru_cache
 def get_settings() -> Settings:
     return Settings()
-
-#jwt 검증
-#권한
-#외부요청 내부요쳥 구분
-#api끼리 소통할때는 인증없이 통과할수있도록
-#토큰 만료처리는 user에서
