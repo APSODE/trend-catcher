ef71a16407ed0b5be32223f6b98554a5928971ff
Neun99 <eoo1010@naver.com>
2026-08-26 01:18:12 +0900

    :recycle: 해시태그 캐시 db에 저장하게 수정

--- src/llm_api/model/__init__.py
+++ src/llm_api/model/__init__.py
@@ -3,3 +3,4 @@ from src.llm_api.model.keyword_model import KeywordModel
 from src.llm_api.model.news_analysis_model import NewsAnalysisModel
 from src.llm_api.model.news_keyword_map_model import NewsKeywordMapModel
 from src.llm_api.model.topic_model import TopicModel
+from src.llm_api.model.search_cache_model import SearchCacheModel
