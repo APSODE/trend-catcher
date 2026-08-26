a4867ef152da8b5bf11f1ecee20bc9b8ab8a2770
APSODE <2gunbo74@gmail.com>
2026-08-27 01:23:28 +0900

    :sparkles: 수정된 Settings 사용 및 요청 인증 로직 수정

--- src/infra/middleware/auth_middleware.py
+++ src/infra/middleware/auth_middleware.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
+import httpx
 from fastapi import Request
 from fastapi.responses import JSONResponse
-from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 
 from src.infra.config.setting import get_settings
 
@@ -8,35 +11,39 @@ from src.infra.config.setting import get_settings
 
 settings = get_settings()
 
-#보안 없는 api
-EXEMPT_PATH_PREFIXES = (
-    "/user/local-login",
-    "/user/social-login",
-    "/user/local-register",
-    "/user/social-register",
-    "/user/refresh",
-    "/health",
-    "/docs",
-    "/openapi.json",
-    "/redoc",
-    "/news/daily",
-    "/article/articles_ids_front",
-)
+class _BearerAuth(httpx.Auth):
+    def __init__(self, token: str):
+        self.token = token
 
+    def auth_flow(self, request):
+        request.headers['Authorization'] = f'{self.token}'
+        yield request
+
+async def _is_valid_token(request: Request) -> bool:
+    jwt = request.headers.get("authorization")
+    if jwt is None:
+        return False
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            url = settings.jwt_check_url,
+            auth = _BearerAuth(jwt)
+        )
+
+        return response.status_code == 200
+
+def _is_unprotected_url(request: Request) -> bool:
+    return request.url.path.startswith(settings.unprotected_url)
 
 class AuthMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
-        path = request.url.path
-        print("authorization path : ", path)
-        if path.startswith(EXEMPT_PATH_PREFIXES):
-            return await call_next(request)
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint):
+        is_protected_url = not _is_unprotected_url(request)
+        is_invalid_token = not await _is_valid_token(request)
 
-        authorization = request.headers.get("Authorization")
-
-        if not authorization:
+        if is_protected_url and is_invalid_token:
             return JSONResponse(
-                status_code=401,
-                content={
+                status_code = 401,
+                content = {
                     "detail": "Unauthorized",
                     "message": "로그인이 필요합니다.",
                 }
