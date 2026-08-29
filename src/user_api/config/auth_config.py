e0ce8045a4809e8af30b3314b017686d58381472
APSODE <2gunbo74@gmail.com>
2026-08-29 22:19:07 +0900

    :sparkles: refresh JWT의 만료시점을 분단위로 변경

--- src/user_api/config/auth_config.py
+++ src/user_api/config/auth_config.py
@@ -7,4 +7,4 @@ class AuthConfig(BaseConfig):
     SECRET_KEY: str = Field(validation_alias = "JWT_SECRET_KEY")
     ALGORITHM: str = Field(validation_alias = "JWT_ALGORITHM")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(validation_alias = "ACCESS_TOKEN_EXPIRE_MINUTES")
-    REFRESH_TOKEN_EXPIRE_DAYS: int = Field(validation_alias = "REFRESH_TOKEN_EXPIRE_MINUTES")
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = Field(validation_alias ="REFRESH_TOKEN_EXPIRE_MINUTES")
