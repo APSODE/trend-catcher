c915a0b7ef7470ff67789ff8d6f3e247aea3711f
jinwoopark <jinwoopark5889@gmail.com>
2026-08-30 14:59:03 +0900

    :sparkles:

--- src/sns_api/model/database_model.py
+++ src/sns_api/model/database_model.py
@@ -16,7 +16,7 @@ engine = create_async_engine(
     echo=settings.db_echo,
     pool_size=settings.db_pool_size,
     max_overflow=settings.db_max_overflow,
-    pool_pre_ping=True,
+    pool_pre_ping=False,
 )
 
 AsyncSessionLocal = async_sessionmaker(
